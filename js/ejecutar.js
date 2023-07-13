2a8b1cd4d5c7c1be43fa8e2c05d47a4c74658853
belen-ramos99 <133837181+belen-ramos99@users.noreply.github.com>
2023-07-13 18:56:40 -0300

    Add files via upload

---
+++ js/ejecutar.js
@@ -0,0 +1,14 @@
+
+function cambiarFondo1(){
+    document.body.style.backgroundColor= "black";
+    document.getElementById("letras").style.color = "whitesmoke";
+    document.getElementById("nav").style.backgroundColor="grey";
+    document.getElementById("titulo").style.color="white";
+    document.getElementById("color").style.color="white"; 
+    document.getElementById("footer").style.color="white"; 
+    
+}
+
+function copy() {
+    alert("Autora: Rocio Belen Ramos. EMAIL: br.ramosbelen@gmail.com");
+    }
